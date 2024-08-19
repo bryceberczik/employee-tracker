582d5248bce28034f33861978aacf02cff69c54d
bryceberczik <berczikbryce@gmail.com>
2024-08-18 22:55:17 -0500

    added new options

--- src/connection.ts
+++ src/connection.ts
@@ -13,7 +13,20 @@ const pool = new Pool({
 const connectToDb = async () => {
   try {
     await pool.connect();
-    console.log('Welcome to Employee Tracker. Enjoy your stay!');
+    console.log('Connected to database successfully!');
+    
+    console.log(`
+  _____                 _                       
+ | ____|_ __ ___  _ __ | | ___  _   _  ___  ___ 
+ |  _| | '_ \` _ \\| '_ \\| |/ _ \\| | | |/ _ \\/ _ \\
+ | |___| | | | | | |_) | | (_) | |_| |  __/  __/
+ |_____|_| |_| |_| .__/|_|\\___/ \\__, |\\___|\\___|
+ |_   _| __ __ _ |_|_| | _____ _|___/           
+   | || '__/ _\` |/ __| |/ / _ \\ '__|            
+   | || | | (_| | (__|   <  __/ |               
+   |_||_|  \\__,_|\\___|_|\\_\\___|_|               
+                                                
+`);
   } catch (err) {
     console.error('Error connecting to database:', err);
     process.exit(1);
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,272 @@
 import inquirer from "inquirer";
 import { QueryResult } from 'pg';
 import { pool, connectToDb } from './connection.js';
-import { getDepartments, getRoles, getManagers, getEmployees } from './helperFunctions.js';
 
 await connectToDb();
+
+const getDepartments = async (): Promise<string[]> => {
+    const query = 'SELECT name FROM department;';
+    const result: QueryResult = await pool.query(query);
+    return result.rows.map(row => row.name);
+};
+
+const getRoles = async (): Promise<string[]> => {
+    const query = 'SELECT title FROM role;';
+    const result: QueryResult = await pool.query(query);
+    return result.rows.map(row => row.title);
+};
+
+const getManagers = async (): Promise<string[]> => {
+    const query = `
+        SELECT first_name || ' ' || last_name AS manager_name
+        FROM employee
+        WHERE manager_id IS NULL;
+    `;
+    const result: QueryResult = await pool.query(query);
+    return result.rows.map(row => row.manager_name);
+};
+
+const getEmployees = async (): Promise<string[]> => {
+    const query = `
+        SELECT e.id, e.first_name || ' ' || e.last_name AS employee_name
+        FROM employee e;
+    `;
+    const result: QueryResult = await pool.query(query);
+    return result.rows.map(row => row.employee_name);
+};
+
+const addRole = async (): Promise<void> => {
+
+    const departments = await getDepartments();
+
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'roleName',
+                message: 'What is the name of the role?'
+            },
+            {
+                type: 'number',
+                name: 'salary',
+                message: 'What is the salary of the role?'
+            },
+            {
+                type: 'list',
+                name: 'department',
+                message: 'Which department does the role belong to?',
+                choices: departments
+            }
+        ])
+        .then((answers) => {
+            const { roleName, salary, department } = answers;
+            const addRoleQuery = `
+            INSERT INTO role (title, salary, department_id)
+            VALUES ($1, $2, (SELECT id FROM department WHERE name = $3));
+        `;
+
+            pool.query(addRoleQuery, [roleName, salary, department], (err: Error, _res: QueryResult) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log(`Added ${roleName} to the database.`);
+                }
+                performActions();
+            });
+        });
+}
+
+const addDepartment = (): void => {
+
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'departmentName',
+                message: 'What is the name of your department'
+            },
+        ])
+        .then((answers) => {
+            const { departmentName } = answers;
+            const addDeptQuery = 'INSERT INTO department (name) VALUES ($1);';
+
+            pool.query(addDeptQuery, [departmentName], (err: Error, _res: QueryResult) => {
+                if (err) {
+
+                    console.error(err);
+                } else {
+
+                    console.log(`Added ${departmentName} to the database.`);
+                    performActions();
+                }
+            })
+
+        })
+}
+
+const addEmployee = async (): Promise<void> => {
+    const managers = await getManagers();
+    const roles = await getRoles();
+
+    const addEmployeeQuery = `
+    INSERT INTO employee (first_name, last_name, role_id, manager_id)
+    VALUES ($1, $2, (SELECT id FROM role WHERE title = $3 LIMIT 1), 
+    (SELECT id FROM employee WHERE first_name = $4 AND last_name = $5 LIMIT 1));
+    `;
+
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'firstName',
+                message: 'What is the employee\'s first name?'
+            },
+            {
+                type: 'input',
+                name: 'lastName',
+                message: 'What is the employee\'s last name?'
+            },
+            {
+                type: 'list',
+                name: 'employeeRole',
+                message: 'What is the employee\'s role?',
+                choices: roles
+            },
+            {
+                type: 'list',
+                name: 'manager',
+                message: 'Who is the employee\'s manager?',
+                choices: [...managers, 'None']
+            }
+        ])
+        .then(async (answers) => {
+            const { firstName, lastName, employeeRole, manager } = answers;
+            let managerFirstName = null;
+            let managerLastName = null;
+
+            if (manager !== 'None') {
+                [managerFirstName, managerLastName] = manager.split(' ');
+            }
+
+            try {
+                await pool.query(addEmployeeQuery, [firstName, lastName, employeeRole, managerFirstName, managerLastName]);
+                console.log('Employee added successfully');
+            } catch (error) {
+                console.error('Error adding employee:', error);
+            }
+            performActions();
+        });
+};
+
+const updateEmployee = async (): Promise<void> => {
+    const employees = await getEmployees();
+    const roles = await getRoles();
+
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'employeeId',
+                message: 'Which employee\'s role do you want to update?',
+                choices: employees
+            },
+            {
+                type: 'list',
+                name: 'roleId',
+                message: 'Which role do you want to assign the selected employee?',
+                choices: roles
+            }
+        ])
+        .then(async (answers) => {
+            const { employeeId, roleId } = answers;
+            const [employeeFirstName, employeeLastName] = employeeId.split(' ')
+            const updateEmployeeRoleQuery = `
+                UPDATE employee
+                SET role_id = (SELECT id FROM role WHERE title = $1 LIMIT 1)
+                WHERE first_name = $2 AND last_name = $3;
+            `;
+
+            pool.query(updateEmployeeRoleQuery, [roleId, employeeFirstName, employeeLastName], (err: Error, _res: QueryResult) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log(`Updated employee's role in the database.`);
+                }
+                performActions();
+            });
+        });
+};
+
+const updateEmployeeManager = async (): Promise<void> => {
+    const employees = await getEmployees();
+    const managers = await getManagers();
+
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'employeeId',
+                message: 'Which employee do you want to update?',
+                choices: employees
+            },
+            {
+                type: 'list',
+                name: 'managerId',
+                message: 'Which manager do you want to assign to the selected employee?',
+                choices: managers
+            }
+        ])
+        .then((answers) => {
+            const { employeeId, managerId } = answers;
+            const [employeeFirstName, employeeLastName] = employeeId.split(' ');
+            const [managerFirstName, managerLastName] = managerId.split(' ');
+            const updateEmployeeManagerQuery = `
+                UPDATE employee
+                SET manager_id = (SELECT id FROM employee WHERE first_name = $1 AND last_name = $2 LIMIT 1)
+                WHERE first_name = $3 AND last_name = $4;
+            `;
+
+            pool.query(updateEmployeeManagerQuery, [managerFirstName, managerLastName, employeeFirstName, employeeLastName], (err: Error, _res: QueryResult) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log(`Updated employee's manager in the database.`);
+                }
+                performActions();
+            });
+        });
+};
+
+const deleteEmployee = (): void => {
+
+    const employees = getEmployees(); 
+
+                inquirer
+                    .prompt([
+                        {
+                            type: 'list',
+                            name: 'employeeId',
+                            message: 'Select an employee to delete:',
+                            choices: employees
+                        }
+                    ])
+                    .then((answers) => {
+                        const { employeeId } = answers;
+                        const [employeeFirstName, employeeLastName] = employeeId.split(' ');
+                        const deleteEmployeeQuery = `
+                            DELETE FROM employee WHERE first_name = $1 AND last_name = $2;;
+                        `;
+            
+                        pool.query(deleteEmployeeQuery, [employeeFirstName, employeeLastName], (err: Error, _res: QueryResult) => {
+                            if (err) {
+                                console.error(err);
+                            } else {
+                                console.log('Employee deleted successfully.');
+                            }
+                            performActions();
+                        });
+                    });
+}
 
 const performActions = (): void => {
 
@@ -21,6 +284,11 @@ const performActions = (): void => {
                     'Add Role',
                     'View All Departments',
                     'Add Department',
+                    'Update Employee Manager',
+                    'Total Utilize Budget',
+                    'Delete Employee',
+                    'Delete Role',
+                    'Delete Department',
                     'Quit'
                 ],
             },
@@ -58,9 +326,9 @@ const performActions = (): void => {
                     });
                     break;
                 case 'Add Employee':
-                    
-                addEmployee();
-                
+
+                    addEmployee();
+
                     break;
                 case 'Update Employee Role':
 
@@ -91,7 +359,7 @@ const performActions = (): void => {
                     break;
                 case 'View All Departments':
 
-                const viewDepartments = `
+                    const viewDepartments = `
                     SELECT department.id as id,
                     department.name as name
                     FROM department;
@@ -109,8 +377,44 @@ const performActions = (): void => {
                     break;
                 case 'Add Department':
 
-                addDepartment();
+                    addDepartment();
 
+                    break;
+                case 'Update Employee Manager':
+
+                    updateEmployeeManager();
+
+                    break;
+                case 'Total Utilize Budget':
+
+                    const totalUtilizeBudgetQuery = `
+                        SELECT d.name AS department, SUM(r.salary) AS total_salary
+                        FROM employee e
+                        JOIN role r ON e.role_id = r.id
+                        JOIN department d ON r.department_id = d.id
+                        GROUP BY d.name;
+                        `;
+
+                    pool.query(totalUtilizeBudgetQuery, (err: Error, res: QueryResult) => {
+                        if (err) {
+                            console.error(err);
+                        } else {
+                            console.table(res.rows);
+                        }
+                        performActions();
+                    });
+                    break;
+                case 'Delete Employee':
+
+                    deleteEmployee();
+                    break;
+                    case 'Delete Role':
+
+
+                    break;
+                    case 'Delete Department':
+
+                
                     break;
                 case 'Quit':
                     process.exit(0);
@@ -122,166 +426,5 @@ const performActions = (): void => {
         });
 }
 
-const addRole = async (): Promise<void> => {
-
-    const departments = await getDepartments();
-
-    inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'roleName',
-            message: 'What is the name of the role?'
-        },
-        {
-            type: 'number',
-            name: 'salary',
-            message: 'What is the salary of the role?'
-        },
-        {
-            type: 'list',
-            name: 'department',
-            message: 'Which department does the role belong to?',
-            choices: departments
-        }
-    ])
-    .then((answers) => {
-        const { roleName, salary, department } = answers;
-        const addRoleQuery = `
-            INSERT INTO role (title, salary, department_id)
-            VALUES ($1, $2, (SELECT id FROM department WHERE name = $3));
-        `;
-
-        pool.query(addRoleQuery, [roleName, salary, department], (err: Error, _res: QueryResult) => {
-            if (err) {
-                console.error(err);
-            } else {
-                console.log(`Added ${roleName} to the database.`);
-            }
-            performActions(); 
-        });
-    });
-}
-
-const addDepartment = (): void => {
-
-    inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'departmentName',
-            message: 'What is the name of your department'
-        },
-    ])
-    .then((answers) => {
-        const { departmentName } = answers;
-        const addDeptQuery = 'INSERT INTO department (name) VALUES ($1);';
-
-        pool.query(addDeptQuery, [departmentName], (err: Error, _res: QueryResult) => {
-            if (err) {
-
-                console.error(err);
-            } else {
-
-                console.log(`Added ${departmentName} to the database.`);
-                performActions();
-            }
-        })
-
-    })
-}
-
-const addEmployee = async (): Promise<void> => {
-    const managers = await getManagers();
-    const roles = await getRoles();
-
-    const addEmployeeQuery = `
-    INSERT INTO employee (first_name, last_name, role_id, manager_id)
-    VALUES ($1, $2, (SELECT id FROM role WHERE title = $3 LIMIT 1), 
-    (SELECT id FROM employee WHERE first_name = $4 AND last_name = $5 LIMIT 1));
-    `;
-
-    inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'firstName',
-            message: 'What is the employee\'s first name?'
-        },
-        {
-            type: 'input',
-            name: 'lastName',
-            message: 'What is the employee\'s last name?'
-        },
-        {
-            type: 'list',
-            name: 'employeeRole',
-            message: 'What is the employee\'s role?',
-            choices: roles
-        },
-        {
-            type: 'list',
-            name: 'manager',
-            message: 'Who is the employee\'s manager?',
-            choices: [...managers, 'None']
-        }
-    ])
-    .then(async (answers) => {
-        const { firstName, lastName, employeeRole, manager } = answers;
-        let managerFirstName = null;
-        let managerLastName = null;
-
-        if (manager !== 'None') {
-            [managerFirstName, managerLastName] = manager.split(' ');
-        }
-
-        try {
-            await pool.query(addEmployeeQuery, [firstName, lastName, employeeRole, managerFirstName, managerLastName]);
-            console.log('Employee added successfully');
-        } catch (error) {
-            console.error('Error adding employee:', error);
-        }
-        performActions();
-    });
-};
-
-const updateEmployee = async (): Promise<void> => {
-    const employees = await getEmployees();
-    const roles = await getRoles();
-
-    inquirer
-        .prompt([
-            {
-                type: 'list',
-                name: 'employeeId',
-                message: 'Which employee\'s role do you want to update?',
-                choices: employees
-            },
-            {
-                type: 'list',
-                name: 'roleId',
-                message: 'Which role do you want to assign the selected employee?',
-                choices: roles
-            }
-        ])
-        .then((answers) => {
-            const { employeeId, roleId } = answers;
-            const [employeeFirstName, employeeLastName] = employeeId.split(' ')
-            const updateEmployeeRoleQuery = `
-                UPDATE employee
-                SET role_id = (SELECT id FROM role WHERE title = $1 LIMIT 1)
-                WHERE first_name = $2 AND last_name = $3;
-            `;
-
-            pool.query(updateEmployeeRoleQuery, [roleId, employeeFirstName, employeeLastName], (err: Error, _res: QueryResult) => {
-                if (err) {
-                    console.error(err);
-                } else {
-                    console.log(`Updated employee's role in the database.`);
-                }
-                performActions();
-            });
-        });
-};
 
 performActions();
